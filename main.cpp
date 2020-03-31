57bf218b95df0b44495f1d21946a99d65e927a0b
Crspy <crspo2020@outlook.com>
2020-03-31 17:36:37 +0200

    clean up

--- main.cpp
+++ main.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
-#include <cstdio>
 #include "Shell.hpp"
 
 int main(int argc, char *argv[])
 {
     Shell sh;
     sh.Run(); // keeps running in a loop till it receives an exit command
-    printf("lmao");
     return EXIT_SUCCESS;
 }
